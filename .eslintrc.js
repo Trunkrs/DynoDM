a25a2830bdcff0e6e8c54e1a757a2b4831e209f1
L.W.F.M. Hertog <loekhertog@gmail.com>
2020-07-09 17:42:37 +0200

    fix import settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
     jest: true,
   },
   rules: {
+    'import/prefer-default-export': ['off'],
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
     'import/no-named-as-default': ['off'],
   },
